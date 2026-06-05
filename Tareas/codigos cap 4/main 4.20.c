af9f9e51f6865c1b76db7ff645da6ae4ed37e05b
manuelarturopuello-hub <manuelarturopuello@gmail.com>
2026-06-05 00:32:43 -0400

    Add files via upload

---
+++ Tareas/codigos cap 4/main 4.20.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+# include <stdio.h>
+
+/* Funciones y parámetros. */
+
+int F1(int , int *);    /* prototipo de funcion. */
+
+int A = 3;
+int B = 7;
+int C = 4;             /* variables globales. */
+int D = 2;
+
+void main(void)
+{
+    A = F1 (C, &D);
+    printf("\n%d %d %d %d" , A, B, C, D);
+    C = 3;
+    C = F1 (A, &C);
+    printf(“\n%d %d %d %d”, A, B, C, D);
+}
+int F1(int X, int *Y)
+{
+int A;
+A = X * *Y;
+C++;
+B += *Y;
+printf(“\n%d %d %d %d”, A, B, C, D);
+*Y--;
+return (C);
+}
